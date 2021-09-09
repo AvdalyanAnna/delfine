1d2e38c1cc6d9db516537e3a236ccc6e34f80509
AvdalyanAnna <anna.avdalyan.12017@gmail.com>
2021-09-09 18:38:43 +0400

    changes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,40 @@ $(function () {
     $('.hamburger').on('click', () => {
         $('.hamburger button').toggleClass('animate')
         $('.nav').toggleClass('active')
-        $('body').toggleClass('overflow-hidden-important')
+        $('#fp-nav ').toggleClass('active-fz')
     })
+
+    if ($('div').is('#map')) {
+        ymaps.ready(init_map_center);
+    }
+
+    function init_map_center() {
+       var  myMap = new ymaps.Map("map", {
+            center: [55.791059392084655, 37.633846758299825],
+            zoom: 10,
+            controls: []
+        });
+        var myPlacemark = new ymaps.Placemark([55.791059392084655, 37.633846758299825], {},
+            {
+                iconLayout: 'default#image',
+                iconImageHref: '/images/icon.png',
+                iconImageSize: [30, 27],
+                iconImageOffset: [-16.5, -46]
+            });
+        myMap.geoObjects.add(myPlacemark);
+        myMap.behaviors.disable('scrollZoom')
+        myMap.events.add('click', function () {
+            myMap.behaviors.enable('scrollZoom');
+        })
+    }
+
+    if ($('div').is('.accordion-head')) {
+        $('.accordion-head').on('click', function () {
+            $(this).parent().toggleClass('accordion-active')
+        })
+    }
+
+
 
     // initiate full page scroll
     if ($('body').is('.home')) {
@@ -11,9 +43,9 @@ $(function () {
             scrollBar: true,
             responsiveWidth: 768,
             navigation: true,
-            // navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
-            // anchors: ["home", "about", "portfolio", "contact", "connect"],
-            // menu: "#myMenu",
+            navigationTooltips: ["Главная", "Уникальность", "Опыт", "Преимущества", "Услуги","Контакты"],
+            anchors: ["section_one", "uniqueness", "an_experience", "advantages", "services","contact"],
+            menu: "#myMenu",
             fitToSection: false,
             afterLoad: (anchorLink, index) => {
                 //using index
@@ -22,9 +54,11 @@ $(function () {
                         /* add opacity to arrow */
                         $('.header').removeClass('header-active')
                         $('.nav').removeClass('nav-active')
+                        $('#fp-nav ').removeClass('active')
                     } else if (index !== 1) {
                         $('.header').addClass('header-active')
                         $('.nav').addClass('nav-active')
+                        $('#fp-nav ').addClass('active')
                     }
                 }
 
@@ -53,26 +87,23 @@ $(function () {
 
     }
 
-    if ($('div').is('.accordion-head')) {
-        $('.accordion-head').on('click', function () {
-            $(this).parent().toggleClass('accordion-active')
-        })
-    }
-    if ($('div').is('.video-list')) {
-        $('.btn-play').on('click', function (e) {
-            e.preventDefault();
-            let iframe = $(this).parent().children('iframe')
 
-            let symbol = iframe[0].src.includes("autoplay=0") > -1
-                ? iframe[0].src.slice(0, -1) + "1"
-                : false;
 
-            let video = iframe[0].src = symbol;
-
-            console.log(video)
-        })
-    }
-
+    // if ($('div').is('.video-list')) {
+    //     $('.btn-play').on('click', function (e) {
+    //         e.preventDefault();
+    //         let iframe = $(this).parent().children('iframe')
+    //
+    //         let symbol = iframe[0].src.includes("autoplay=0") > -1
+    //             ? iframe[0].src.slice(0, -1) + "1"
+    //             : false;
+    //
+    //         let video = iframe[0].src = symbol;
+    //
+    //         console.log(video)
+    //     })
+    // }
+    //
 
     //
     // // fullpage.js link navigation
